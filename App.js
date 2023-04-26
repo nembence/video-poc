7aec390b4c41a58d057cca8685f7055bf9c53e5b
Bence Nemeth <57453541+nembence@users.noreply.github.com>
2023-04-26 14:34:54 +0200

    finish the implementation with react-native-video

--- App.js
+++ App.js
@@ -1,42 +1,132 @@
-import { useRef, useEffect } from "react";
-import { StyleSheet, TouchableOpacity, View, Text } from "react-native";
+import { Icon } from "@rneui/themed";
+import { useRef, useState } from "react";
+import {
+  StyleSheet,
+  TouchableOpacity,
+  View,
+  Text,
+  ScrollView,
+  Image,
+} from "react-native";
 import Video from "react-native-video";
 
 export default function App() {
   const video = useRef(null);
+  const [paused, setPaused] = useState(true);
 
-  const onPlay = () => video.current.presentFullscreenPlayer();
+  const onPlay = () => {
+    setPaused(false);
+    setTimeout(() => video.current.presentFullscreenPlayer(), 50);
+  };
 
   return (
-    <View style={styles.container}>
-      <Video
-        ref={video}
-        source={{
-          uri: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
-        }}
-        fullscreen
-        resizeMode={"cover"}
-        style={styles.backgroundVideo}
-      />
-      <TouchableOpacity onPress={onPlay}>
-        <Text>Play</Text>
-      </TouchableOpacity>
+    <View style={{ flex: 1 }}>
+      <ScrollView contentContainerStyle={styles.container}>
+        <View>
+          <Text style={styles.title}>Video PoC</Text>
+        </View>
+        <View>
+          <Text style={styles.subtile}>Video with player icon</Text>
+        </View>
+        <View style={styles.videoPlayer}>
+          {paused ? (
+            <Image
+              style={styles.videoPlayer}
+              source={require("./assets/cover-image.png")}
+            />
+          ) : (
+            <Video
+              ref={video}
+              source={{
+                uri: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+              }}
+              resizeMode={"cover"}
+              style={styles.backgroundVideo}
+              onFullscreenPlayerWillDismiss={() => {
+                setPaused(true);
+              }}
+              muted
+              fullscreen
+              paused={paused}
+            />
+          )}
+
+          <TouchableOpacity onPress={onPlay} style={styles.iconContainer}>
+            <Icon name="play-circle-fill" color="white" size={60} />
+          </TouchableOpacity>
+        </View>
+        <View>
+          <Text style={styles.subtile}>Repeated video in background</Text>
+        </View>
+        <View style={styles.videoPlayer}>
+          <Video
+            ref={video}
+            source={{
+              uri: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+            }}
+            fullscreen
+            repeat
+            resizeMode={"cover"}
+            style={styles.backgroundVideo}
+            rate={1}
+            paused={false}
+            muted
+            playInBackground={true}
+          />
+          <View style={styles.videoTitleContainer}>
+            <Text style={styles.videoTitle}>Stage 1: Baseline</Text>
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 }
-
-// https://manifest.prod.boltdns.net/manifest/v1/hls/v4/clear/1336131408001/a691e6e3-f7f1-42e2-b00d-5d87dee5a0ac/10s/master.m3u8?fastly_token=NjQ0OTQ1MGZfMzdhOGYwZTY1MzRkZmI0MzY4MWI1MDQ3Zjk3YWJlY2EyMWI4YzQ0MDU5MGEyNmRmNjFmOWQwYTIxYTNmM2NlZg%3D%3D
-
-// http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
+    paddingBottom: 30,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+    marginBottom: 20,
+    marginTop: 80,
+  },
+  subtile: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 20,
+    marginTop: 20,
+  },
+  iconContainer: {
+    position: "absolute",
+    top: "45%",
+    left: "45%",
+  },
+  icon: {
+    width: 100,
+    height: 100,
+  },
+
+  videoPlayer: {
+    position: "relative",
+    width: "100%",
+    height: 500,
   },
   backgroundVideo: {
     height: 500,
     width: "100%",
+  },
+  videoTitleContainer: {
+    position: "absolute",
+    top: 30,
+    left: 20,
+  },
+  videoTitle: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "white",
   },
 });
